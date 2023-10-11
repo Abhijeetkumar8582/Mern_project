c40815d74e4f8e9ab3d4972aa29cc6bb7187729d
Abhijeet kumar <abhijeet122kumar@gmail.com>
2023-10-11 19:07:47 +0530

    production error checking

---
+++ .next/server/chunks/368.js
@@ -0,0 +1,132 @@
+"use strict";
+exports.id = 368;
+exports.ids = [368];
+exports.modules = {
+
+/***/ 4368:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "fitnessTags": () => (/* binding */ fitnessTags),
+/* harmony export */   "healthTags": () => (/* binding */ healthTags),
+/* harmony export */   "nutritionTags": () => (/* binding */ nutritionTags),
+/* harmony export */   "yogaBenefits": () => (/* binding */ yogaBenefits),
+/* harmony export */   "yogaTags": () => (/* binding */ yogaTags)
+/* harmony export */ });
+const yogaTags = [
+    "Yoga",
+    "YogaPoses",
+    "Meditation",
+    "YogaLife",
+    "HealthyLiving",
+    "Wellness",
+    "Fitness",
+    "Mindfulness",
+    "YogaJourney",
+    "YogaCommunity",
+    "YogaPractice",
+    "YogaInspiration",
+    "YogaFlow",
+    "YogaTeacher",
+    "YogaEveryday",
+    "StressRelief",
+    "Flexibility",
+    "Strength",
+    "Balance",
+    "InnerPeace"
+];
+const nutritionTags = [
+    "Nutrition",
+    "HealthyEating",
+    "Diet",
+    "NutritionTips",
+    "NutritionFacts",
+    "BalancedDiet",
+    "NutritionAdvice",
+    "HealthyDiet",
+    "NutritionPlan",
+    "EatingWell",
+    "NutritionEducation",
+    "MealPlanning",
+    "NutritionalWellness",
+    "WholeFoods",
+    "NutritionGoals",
+    "Nutritionist",
+    "CleanEating",
+    "NutritionForLife",
+    "HealthyChoices",
+    "FoodAsMedicine"
+];
+const fitnessTags = [
+    "Fitness",
+    "FitnessJourney",
+    "Workout",
+    "Exercise",
+    "HealthyLifestyle",
+    "StrengthTraining",
+    "Cardio",
+    "GymLife",
+    "FitLife",
+    "FitnessMotivation",
+    "FitnessGoals",
+    "ActiveLifestyle",
+    "HealthAndFitness",
+    "Weightlifting",
+    "CrossFit",
+    "Running",
+    "Bodybuilding",
+    "Wellness",
+    "Nutrition",
+    "FitnessCommunity"
+];
+const healthTags = [
+    "Health",
+    "HealthyLiving",
+    "Wellness",
+    "Healthcare",
+    "HealthTips",
+    "HolisticHealth",
+    "MentalHealth",
+    "PhysicalHealth",
+    "WellBeing",
+    "SelfCare",
+    "HealthyChoices",
+    "MindBodyHealth",
+    "HealthAndWellness",
+    "HealthyHabits",
+    "Nutrition",
+    "HealthyMind",
+    "HealthyBody",
+    "WellnessJourney",
+    "FitnessForHealth",
+    "HealthyLifestyle"
+];
+const yogaBenefits = [
+    "Improved flexibility",
+    "Increased strength",
+    "Stress reduction",
+    "Enhanced mental clarity",
+    "Better posture and alignment",
+    "Mind-body connection",
+    "Relaxation",
+    "Inner peace",
+    "Weight management",
+    "Improved balance",
+    "Cardiovascular health",
+    "Respiratory health",
+    "Pain relief",
+    "Flexibility",
+    "Muscle tone",
+    "Mood enhancement",
+    "Better sleep",
+    "Digestive health",
+    "Energy boost",
+    "Spiritual growth"
+];
+
+
+/***/ })
+
+};
+;
